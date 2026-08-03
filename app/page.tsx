a9be171b85fcc3e157fb8b924dfe28b01e00a0c9
up <up@ups-MacBook-Air.local>
2026-08-03 16:49:00 +0700

    Test deployment tracking - modify home page

--- app/page.tsx
+++ app/page.tsx
@@ -9,6 +9,7 @@ import Contact from "@/components/Contact";
 import Footer from "@/components/Footer";
 
 export default function Home() {
+  // Test deployment - version 2
   return (
     <>
       <Header />
